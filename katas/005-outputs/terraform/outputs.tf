a2f9820554d128daaf1fc64b2db60afb435ab71e
satoru-o <strieepiclearner@gmail.com>
2026-09-11 23:09:35 +0900

    feat: 005回答

---
+++ katas/005-outputs/terraform/outputs.tf
@@ -0,0 +1,14 @@
+output "instance_id" {
+  description = "EC2 Instance ID"
+  value = aws_instance.dojo-005-ec2.id
+}
+
+output "public_ip" {
+  description = "Web IP"
+  value = aws_instance.dojo-005-ec2.public_ip
+}
+
+output "security_group_id" {
+  description = "Security Group for Web Server"
+  value = aws_security_group.dojo-005-sg.id
+}
